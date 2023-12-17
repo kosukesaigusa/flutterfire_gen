bb32f77f484ff648f700f814f413aab0242eb769
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-12-17 16:55:28 +0900

    feat: add read, create, update, delete class prefix option to FirestoreDocument annotation

--- packages/flutterfire_gen_annotation/lib/src/firestore_document.dart
+++ packages/flutterfire_gen_annotation/lib/src/firestore_document.dart
@@ -3,6 +3,10 @@ class FirestoreDocument {
   /// Creates a [FirestoreDocument].
   const FirestoreDocument({
     required this.path,
+    this.readClassPrefix,
+    this.createClassPrefix,
+    this.updateClassPrefix,
+    this.deleteClassPrefix,
     this.includePathField,
     this.includeDocumentReferenceField,
     this.generateCopyWith,
@@ -10,6 +14,22 @@ class FirestoreDocument {
 
   /// A path to firestore document.
   final String path;
+
+  /// The prefix for the class name of entities created based on documents read
+  /// from Cloud Firestore.
+  final String? readClassPrefix;
+
+  /// The prefix for the class name of data structure for creating a new
+  /// document in Cloud Firestore.
+  final String? createClassPrefix;
+
+  /// The prefix for the class name of data structure for updating a document in
+  /// Cloud Firestore.
+  final String? updateClassPrefix;
+
+  /// The prefix for the class name of data structure for deleting a  document
+  /// in Cloud Firestore.
+  final String? deleteClassPrefix;
 
   /// Whether to include `path` field in ReadEntity class.
   final bool? includePathField;
